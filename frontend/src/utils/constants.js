66f7fb36c18206d1dba95327e0ebdbbd6d09b3b2
Ans Sattar <anssattar33@gmail.com>
2025-05-02 23:13:11 +0500

    01 changes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user"
-export const TWEET_API_END_POINT = "http://localhost:3000/api/v1/tweet"
+export const USER_API_END_POINT = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/v1/user`
+export const TWEET_API_END_POINT = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/v1/tweet`
